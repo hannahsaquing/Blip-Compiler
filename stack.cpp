0ffd4347fdcd616de3b260620448e088df041926
Hannah <hannah.saquing@utexas.edu>
2021-05-03 12:14:14 -0500

    psuedo code

---
+++ stack.cpp
@@ -0,0 +1,23 @@
+#include <stack>
+#include <map>
+#include <string>
+
+void instructionReader() {
+    /*
+     * interpret the instruction that will need to be used
+     */
+}
+
+void addSymbolTable() {
+    /*
+     * add the variable to the symbol table
+     */
+}
+
+void updateSymbolTable() {
+    /*
+     * update existing variable to the symbol table
+     */
+}
+
+void 
