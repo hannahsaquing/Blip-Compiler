6c224510a8d58fc0ebcb711884c13f3c03a73099
Hannah <hannah.saquing@utexas.edu>
2021-05-03 12:04:53 -0500

    restart

---
+++ cmake-build-debug/CMakeFiles/Project8_stack.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Project8_stack.dir/main.cpp.o"
+  "Project8_stack.exe"
+  "Project8_stack.pdb"
+  "libProject8_stack.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Project8_stack.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
