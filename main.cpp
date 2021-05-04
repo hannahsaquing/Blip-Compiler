4e84530645735ed00e4ee62261f0e89765a6fc34
Hannah <hannah.saquing@utexas.edu>
2021-05-04 13:11:01 -0500

    main file set to grading script

--- main.cpp
+++ main.cpp
@@ -6,25 +6,7 @@
 #include "stack.h"
 #include "blip.h"
 
-int main() {
-    cout << "Start of Test 1" << endl;
-    set_input("test1.blip");
+int main(void) {
+    set_input("test_grader.blip");
     run();
-    cout << endl << "----------- end of Test 1 -----------" << endl;
-
-    cout << "Start of Test 2" << endl;
-    set_input("test2.blip");
-    run();
-    cout << endl << "----------- end of Test 2 -----------" << endl;
-
-    cout << "Start of Test 3" << endl;
-    set_input("test3.blip");
-    run();
-    cout << endl << "----------- end of Test 3 -----------" << endl;
-
-    cout << "Start of Test 4" << endl;
-    set_input("test4.blip");
-    run();
-    cout << endl << "----------- end of Test 4 -----------" << endl;
-
 }
